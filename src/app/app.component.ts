f212f23b7c2c0d21daac770eb179ca57efcdb625
Llubitza-Linares <llubylinares@gmail.com>
2021-08-12 18:19:57 -0400

    12/8/2021 Observer Of

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {of} from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -13,8 +15,34 @@ export class AppComponent {
 
   color:string;
 
+  tiktok = of([1,2,3,4,5,6]);
+
   constructor(){
+    //PERSON A
+    this.tiktok.pipe(
+      map(s => s.join('-')),
+      map(s => s + 'hola'),
+  
+    ).subscribe(v => {
+      console.log('PERSON A VIDEO', v);
+    });
+    //PERSON B
+    this.tiktok.pipe(
+      filter((v:any) => v[0]%2 ===1)
+    ).subscribe(v => {
+      console.log('PERSON A VIDEO', v);
+    });
+    //PERSON C
+    this.tiktok.subscribe(v => {
+      console.log('PERSON A VIDEO', v);
+    });
   }
+
+onAddVideo(){
+  this.tiktok =of([6,7,8,9,10])
+}
+
+
 
 printDataRicardoComp(event:any){
   console.log('RICARDO COMP:', event);
