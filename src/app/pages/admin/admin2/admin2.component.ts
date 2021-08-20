1c00a8ea39b53d1ea934f27fba3e916efc71d44a
Llubitza-Linares <llubylinares@gmail.com>
2021-08-20 17:39:24 -0400

    20/8/2021 first task- first pipe built

--- src/app/pages/admin/admin2/admin2.component.ts
+++ src/app/pages/admin/admin2/admin2.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-admin2',
   template: `
@@ -29,15 +30,15 @@ export class Admin2Component implements OnInit {
   ngOnInit() {
   }
 
-  /** 
-   * @params name 
-   * @params lastName 
+  /**
+   * @params name
+   * @params lastName
    * return name and lastName joined
   */
-  public testParams(name:string, lastName: string = 'gutierrez'): string{
+  public testParams(name:string, lastName?: string): string{
     const aux = 'juan';
     console.log((name ?? aux)  + ' ' + lastName)
     return name + ' ' + lastName;
   }
 
-} 
+}
