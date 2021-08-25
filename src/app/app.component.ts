ff7d12cbb229e3a2c8b9c5ecd76aac451c0964ef
Llubitza-Linares <llubylinares@gmail.com>
2021-08-24 20:59:43 -0400

    24/5/2021 task function pure and impure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
-import {BehaviorSubject, of, Subscription} from 'rxjs';
-import { delay, filter, map } from 'rxjs/operators';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { BehaviorSubject, of, Subscription } from 'rxjs';
+import { filter, map, delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,80 +8,24 @@ import { delay, filter, map } from 'rxjs/operators';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular2021';
-  myStatus = 'my status';
+  constructor() {
+    this.pure(2, 3);
+    this.pure(10, 2);
+    this.pure(5, 5);
 
-  sw = true;
-
-  color:string;
-  personASub:Subscription;
-  video = 1;
-  tictock = new BehaviorSubject(this.video);
-
-  @ViewChild('myDiv1')myDiv1:ElementRef;
-
-  @ViewChild('myDiv1')myDiv2:ElementRef;
-
-  @ViewChild('myCompLlubitza') myCompLlubitza: any;
-
-  @ViewChild("myDiv3") myDiv3 = ElementRef;
-
-  constructor(){
-
-    // PERSON A
-    this.personASub = this.tictock.pipe(
-      filter(s => s%2 === 0)
-    ).subscribe(v => {
-      console.log('PERSON A VIDEO', v);
-    });
-    // PERSON B
-    this.tictock.pipe(
-      delay(4000)
-    ).subscribe(v => {
-      console.log('PERSON B VIDEO', v);
-    });
-    // PERSON C
-    this.tictock.subscribe(v => {
-      console.log('PERSON C VIDEO', v);
-    });
-  }
-  onAddVideo(){
-    this.video ++
-    this.tictock.next(this.video);    
+    this.impure(2, 3);
+    this.impure(10, 2);
+    this.impure(5, 5);
   }
 
-  person1Unsubscribe(){
-     this.personASub.unsubscribe();
-     console.log('PERSON A SE DESUSCRIBE')
+  pure(a: number, b: number) {
+    console.log(a + b);
+    return a + b;
   }
 
-printDataRicardoComp(event:any){
-  console.log('RICARDO COMP:', event);
-}
-
-
-printData(event){
-  console.log('CHILD COMP SEND DATA: ', event);
-}
-  
-
-test(event){
-  console.log('AAA<zx<zAAAAAAAAAAAAAAAAAAAAAAAAA', event);
-}
-
-test100(event){
-  console.log('234234234234234', event);
-}
-
-onShowLocalVars(){
-  console.log(this.myDiv1, this.myDiv2, this.myCompLlubitza);
-
-  this.myCompLlubitza.onClickTest();
-
-  this.myDiv1.nativeElement.style.backgroundColor ='green';
-console.log(this.myDiv1, this.myDiv2, this.myCompLlubitza);
-
-
-  this.myDiv2.nativeElement.value = 'llubitza';
-}
+  impure(a: number, b: number) {
+    const aux = Math.random();
+    console.log(a + b + aux);
+    return a + b + aux;
+  }
 }
