b9ea10fb5d5596a5f7857c09779b3c39d542624c
Llubitza-Linares <llubylinares@gmail.com>
2021-08-25 18:04:00 -0400

    25/8/2021 task Component Navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { Directive1Directive } from './directive1.directive';
 import { Directive2Directive } from './directive2.directive';
@@ -11,6 +12,21 @@ import { SharedModule } from './shared/shared.module';
 import { Test1Component } from './test1/test1.component';
 import { Test2Component } from './test2/test2.component';
 import { Test3Component } from './test3/test3.component';
+
+import { View1Component } from './view1/view1.component';
+import { View2Component } from './view2/view2.component';
+
+const routes:Routes =[
+  {
+    path:'', redirectTo: 'view1', pathMatch:'full'
+  }
+  ,{
+    path:'view1', component: View1Component
+  },
+  {
+    path:'view2', component: View2Component
+  }
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +34,9 @@ import { Test3Component } from './test3/test3.component';
     Test2Component,
     Test3Component,
     Directive1Directive,
-    Directive2Directive
+    Directive2Directive,
+    View1Component,
+    View2Component
   ],
   imports: [
     BrowserModule,
@@ -26,7 +44,9 @@ import { Test3Component } from './test3/test3.component';
     HomeModule,
     AdminModule,
     BuyModule,
-    SharedModule
+    SharedModule, 
+    RouterModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent],
