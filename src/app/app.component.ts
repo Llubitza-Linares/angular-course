09cceb8067439fae56762c2a75a2390f889c33b5
Llubitza-Linares <llubylinares@gmail.com>
2021-08-26 18:37:47 -0400

    26/8/2021 task sending params in navigation, listneing params of navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import {BehaviorSubject, of, Subscription} from 'rxjs';
 import { filter, map, delay } from 'rxjs/operators';
+import {Router} from "@angular/router";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,26 +11,35 @@ import { filter, map, delay } from 'rxjs/operators';
 
 export class AppComponent {
 
+  id:number = 3;
   data = [1,2,3,4,5,6,7,8,9];
 
-  constructor() {
-    this.pure(2, 3);
-    this.pure(10, 2);
-    this.pure(5, 5);
+  constructor(private router: Router){
+    this.pure(2,3);
+    this.pure(10,2);
+    this.pure(5,5);
 
-    this.impure(2, 3);
-    this.impure(10, 2);
-    this.impure(5, 5);
+
+    this.impure(2,3);
+    this.impure(10,2);
+    this.impure(5,5);
   }
 
-  pure(a: number, b: number) {
-    console.log(a + b);
+
+  pure(a:number, b:number){
+    console.log(a + b)
     return a + b;
   }
 
-  impure(a: number, b: number) {
+  impure(a:number, b:number){
     const aux = Math.random();
-    console.log(a + b + aux);
+    console.log( a + b + aux)
     return a + b + aux;
   }
+
+  onGoView2FromTS():void{
+    this.router.navigate(['view2', this.id, 'sub', 33333])
+  }
+
+
 }
--- src/app/view2/view2.component.ts
+++ src/app/view2/view2.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-view2',
   templateUrl: './view2.component.html',
-  styleUrls: ['./view2.component.css']
+  styleUrls: ['./view2.component.scss']
 })
 export class View2Component implements OnInit {
 
-  constructor() { }
+  constructor(private router: ActivatedRoute) {
 
-  ngOnInit() {
+  }
+
+  ngOnInit(): void {
+
+    /*this.router.params.subscribe(p => {
+      console.log('PARAMS', p);
+    });*/
+
+    console.log('SNAPSHOT', this.router.snapshot.params)
+
+
+
+
+
+
   }
 
 }
