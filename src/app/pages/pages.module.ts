0f863349fad824d43c3128972bf5b8e65372e7e9
Llubitza-Linares <llubylinares@gmail.com>
2021-08-31 16:27:43 -0400

    30/8/2021 Configuration of Angular -correct

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,9 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:'', redirectTo:'home', pathMatch:'full'}
-  ,{path:'home', loadChildren:() => import('./home/home.module').then(m=>m.HomeModule)},
-  {path:'profile', loadChildren:() => import('./profile/profile.module').then(m=>m.ProfileModule)},
+
 ];
 
 @NgModule({
