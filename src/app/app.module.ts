d9317f365d91c1265ad8e1bfbc2143f6bd9c7586
Llubitza-Linares <llubylinares@gmail.com>
2021-08-31 15:49:05 -0400

    30/8/2021 Configuration of Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
 
 const routes: Routes = [
-  {path:'', redirectTo:'login',pathMatch:'full'},
-  {path:'login', loadChildren: ()=> import('./login/login.module').then(m => m.LoginModule)},
-  {path:'pages', loadChildren: ()=> import('./pages/pages.module').then(m => m.PagesModule)},
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', loadChildren:
+      () => import('./login/login.module').then(m => m.LoginModule)},
+  {path: 'pages', loadChildren:
+      () => import('./pages/pages.module').then(m => m.PagesModule)}
 ];
 
 @NgModule({
   declarations: [
-    AppComponent,
-
+    AppComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,24 +1,28 @@
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
-import { RouterModule, Routes } from '@angular/router';
+import {RouterModule, Routes} from "@angular/router";
 import {FormsModule} from "@angular/forms";
 import {NgbButtonsModule, NgbModule, NgbToastModule} from "@ng-bootstrap/ng-bootstrap";
+import {MatSliderModule} from "@angular/material/slider";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
-const routes: Routes=[
+const routes: Routes = [
   {path: '', component: LoginComponent}
-];
+]
 
 @NgModule({
+  declarations: [
+    LoginComponent
+  ],
   imports: [
+    MatProgressSpinnerModule,
     NgbToastModule,
     NgbButtonsModule,
+    MatSliderModule,
     CommonModule,
     FormsModule,
     RouterModule.forChild(routes)
-  ],
-  declarations: [
-    LoginComponent,
-
   ]
 })
-export class LoginModule { } 
+export class LoginModule { }
