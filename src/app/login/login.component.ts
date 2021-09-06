566ea4a1b30a795191e5016dd532756a4bb00e13
Llubitza-Linares <llubylinares@gmail.com>
2021-09-06 01:04:55 -0400

    3/9/2021 reactive form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { PublicationService } from './services/publication.service';
 import { SingletonService } from './services/singleton.service';
 import { Test1Service } from './services/test1.service';
+
 
 @Component({
   selector: 'app-login',
@@ -10,25 +12,44 @@ import { Test1Service } from './services/test1.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private testService:Test1Service, private singletonService: SingletonService,
-    private publicationService: PublicationService) {
-    console.log(this.testService.getItems());
+  name = new FormControl('');
+
+
+  formReactive: FormGroup;
+
+  constructor(private test1Service: Test1Service,
+              private formBuilder: FormBuilder,
+              private singletonService: SingletonService,
+              private publicationService: PublicationService) {
+    console.log(this.test1Service.getItems())
+
+    this.formReactive = this.formBuilder.group({
+      name: '',
+      lastName: ['', [Validators.required]],
+      date: ''
+    });
+
   }
 
   ngOnInit(): void {
+
+    this.formReactive.valueChanges.subscribe(res => {
+      console.log('FORM REACTOVE', res);
+    })
+
+    this.name.valueChanges.subscribe(res => {
+      console.log('CHANGES', res)
+    });
 
     this.publicationService.getAll()
       .subscribe(res => {
         console.log('RESPONSE: ', res);
       });
   }
-
-  setMessage(){
-    this.singletonService.setMessage("hi form login")
+  setMessage() {
+    this.singletonService.setMessage('HI FROM LOGIN');
   }
   update() {
-
-
     this.publicationService.update('p0001',{
       "date": "17/82/2021",
       "description": "update from frontend",
@@ -38,9 +59,7 @@ export class LoginComponent implements OnInit {
     } ).subscribe(
       res => console.log(res))
   }
-
   create(){
-
     this.publicationService.create({
       "date": "17/82/2021",
       "description": "soy nuevo",
@@ -49,23 +68,28 @@ export class LoginComponent implements OnInit {
       "userPhotoUrl": "http://fitebase/user1.jpg"
     } ).subscribe(
       res => console.log(res))
-
   }
-
   delete(){
     this.publicationService.delete('p0002').subscribe(
       res => console.log(res))
   }
-
   patch() {
     this.publicationService.patch('p0001',{
       "description": "update from frontend patch",
     } ).subscribe(
       res => console.log(res))
   }
-
   onSubmitTemplate(values:any){
     console.log('VALUES: ', values)
+  }
+
+
+  onShow(){
+    console.log(this.name.value)
+  }
+
+  onShowAll(){
+    console.log('DDDDD', this.formReactive.value)
   }
 
 
