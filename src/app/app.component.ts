d5fdfd3faff80bd9be93eb2d75c43072cb1ad339
Llubitza-Linares <llubylinares@gmail.com>
2021-09-07 18:11:19 -0400

    7/9/2021 task 1,2,3,4,8,9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { TransactionsService } from './services/transactions.service';
+import { WalletsService } from './services/wallets.service';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <router-outlet></router-outlet>
-  `
+  templateUrl: './app.component.html'
 })
-
 export class AppComponent {
+  title = 'test';
 
+  wallets:any[]= [];
+  transactions:any[] = [];
+  walletSubscription: Subscription;
+  transactionsSubscription: Subscription;
+
+  constructor(
+    private walletsService: WalletsService,
+    private transactionsService: TransactionsService
+  ) {}
+
+  ngOnInit() {
+    this.getTransaction();
+    this.getWallet();
+  }
+
+  getTransaction() {
+    this.transactions = [];
+    this.transactionsSubscription = this.transactionsService
+      .getTransactions()
+      .subscribe(res => {
+        Object.entries(res).map((p: any) =>
+          this.transactions.push({ id: p[0], ...p[1] })
+        );
+      });
+  }
+
+  getWallet() {
+    this.wallets = [];
+    this.walletSubscription = this.walletsService.getWallets().subscribe(res => {
+      Object.entries(res).map((p: any) =>
+        this.wallets.push({ id: p[0], ...p[1] })
+      );
+    });
+  }
+
+  mine() {}
 }
