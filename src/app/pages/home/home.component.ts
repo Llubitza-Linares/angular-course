76d78e0cffb0142dc6c3a4a96e03bff73100cccb
Llubitza-Linares <llubylinares@gmail.com>
2021-09-10 18:45:48 -0400

    10/9/2021 task 2 correct-publication service, catch error interceptor, verification token

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {environment} from "../../../../environments/environment";
+
 import {Observable} from "rxjs";
+import { environment } from '../../../environments/environment.prod';
 
 @Injectable()
 export class PublicationService {
